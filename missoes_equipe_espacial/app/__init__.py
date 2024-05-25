738598fad422fba310a4c72fd55d8518ca9288f7
Tiago Aragão <tiagoaragaobr@gmail.com>
2024-05-25 18:40:44 -0300

    codigo da pagina __init__.py

--- missoes_equipe_espacial/app/__init__.py
+++ missoes_equipe_espacial/app/__init__.py
@@ -11,7 +11,15 @@ api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+from app.model.missao import Missao
 
 with app.app_context:
     db.create_all()
+
+from app.view.resMissoes import Index,CriarMissao,AtualizarMissao,DeletarMissao
+api.add_resource(Index, '/')
+api.add_resource(CriarMissao, '/criar')
+api.add_resource(AtualizarMissao, '/atualizar')
+api.add_resource(DeletarMissao, '/deletar')
+
 
